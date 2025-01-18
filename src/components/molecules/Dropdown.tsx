f2186e392ab12c9f3a6987ea4ff0f7c25de15512
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2025-01-19 03:09:56 +0700

    fix: animation

--- src/components/molecules/Dropdown.tsx
+++ src/components/molecules/Dropdown.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/lib/utils';
 import { Menu } from '@/types';
 import Button from '../atoms/Button';
 import NavigationItem from './NavigationItem';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 interface Props {
   label: string;
@@ -13,12 +14,16 @@ interface Props {
 }
 
 export default function Dropdown({ label, items = [] }: Props) {
+  const [parentRef] = useAutoAnimate({
+    duration: 200,
+  });
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
-    <div className="relative">
+    <div ref={parentRef} className="relative">
       <Button
         variant="ghost"
         className="gap-2 px-4 text-white"
@@ -27,33 +32,32 @@ export default function Dropdown({ label, items = [] }: Props) {
         onMouseLeave={() => setIsOpen(false)}>
         {label} <Icons.chevronDown className={cn('transition-transform', isOpen ? 'rotate-180' : '')} />
       </Button>
-      <div
-        className={cn(
-          'absolute left-0 top-[90%] z-10 mt-1 flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-lg transition-all',
-          !isOpen && 'h-0 border-0 opacity-0',
-        )}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}>
-        {items.map((item, index) => {
-          if (item.isHeading) {
-            return (
-              <span key={index} className="px-4 py-2.5">
-                {item.label}
-              </span>
-            );
-          }
+      {isOpen && (
+        <div
+          className="absolute left-0 top-[90%] z-10 mt-1 flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-lg"
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}>
+          {items.map((item, index) => {
+            if (item.isHeading) {
+              return (
+                <span key={index} className="px-4 py-2.5">
+                  {item.label}
+                </span>
+              );
+            }
 
-          return (
-            <Fragment key={index}>
-              {item.isFooter && <hr />}
-              <NavigationItem
-                menu={item}
-                className="text-neutral w-full justify-start rounded-none py-2.5 font-normal hover:bg-slate-300/25"
-              />
-            </Fragment>
-          );
-        })}
-      </div>
+            return (
+              <Fragment key={index}>
+                {item.isFooter && <hr />}
+                <NavigationItem
+                  menu={item}
+                  className="text-neutral w-full justify-start rounded-none py-2.5 font-normal hover:bg-slate-300/25"
+                />
+              </Fragment>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/organisms/Requirements.tsx
+++ src/components/organisms/Requirements.tsx
@@ -1,21 +1,39 @@
+'use client';
+
 import Link from 'next/link';
 import Requirement from '../molecules/Requirement';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { useState, useEffect } from 'react';
 
 const requirements = ['한국어 능력', '업무 수행 능력', '겸업 여부', '평판 조회'];
 
 export default function Requirements() {
+  const [parentRef] = useAutoAnimate({
+    duration: 500,
+  });
+
+  const [showRequirements, setShowRequirements] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShowRequirements(true), 50);
+  }, []);
+
   return (
-    <div className="mt-6 space-y-4 px-4 xl:hidden">
-      <div className="grid w-72 grid-cols-2 gap-2">
-        {requirements.map((requirement) => (
-          <Requirement key={requirement}>{requirement}</Requirement>
-        ))}
-      </div>
-      <div>
-        <Link href="#" className="text-accent underline">
-          개발자가 필요하신가요?
-        </Link>
-      </div>
+    <div ref={parentRef} className="mt-6 space-y-4 px-4 xl:hidden">
+      {showRequirements && (
+        <>
+          <div className="grid w-72 grid-cols-2 gap-2">
+            {requirements.map((requirement) => (
+              <Requirement key={requirement}>{requirement}</Requirement>
+            ))}
+          </div>
+          <div>
+            <Link href="#" className="text-accent underline">
+              개발자가 필요하신가요?
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
